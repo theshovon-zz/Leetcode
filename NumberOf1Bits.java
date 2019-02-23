13925c525b475d7a61c70f148f54e309946fc9ef
Shovon Saha <mxsaha5@gmail.com>
2019-02-23 11:08:50 -0500

    Create NumberOf1Bits.java

---
+++ NumberOf1Bits.java
@@ -0,0 +1,14 @@
+public class NumberOf1Bits{
+    public static int hammingWeight(int n) {
+         int counter = 0;
+          for(int i = 0; i < 32; i++ ){
+            int mask =  1 << i;
+            int masked_n = n & mask;
+            int thebit = masked_n >>> i;
+            if(thebit == 1){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
